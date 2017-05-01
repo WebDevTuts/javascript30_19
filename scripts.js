f590371f80f25b4a399be162494cb0a15ecca718
Edwin Chen <edwinchen85@gmail.com>
2017-05-01 17:34:08 +0800

    Get the pixels information from the canvas

--- scripts.js
+++ scripts.js
@@ -24,6 +24,7 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    let pixels = ctx.getImageData(0, 0, width, heigth);
   }, 16);   // start every 16ms
 }
 
