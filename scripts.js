d4ed65e41ad0f923aac6573cd2c61ecb7a123632
Edwin Chen <edwinchen85@gmail.com>
2017-05-01 17:39:42 +0800

    Define redEffect function. Apply the red effect to the pixels in the canvas.

--- scripts.js
+++ scripts.js
@@ -24,7 +24,12 @@ function paintToCanvas() {
 
   return setInterval(() => {
     ctx.drawImage(video, 0, 0, width, height);
+    // take the pixels out
     let pixels = ctx.getImageData(0, 0, width, heigth);
+    // mess with them
+    pixels = redEffect(pixels);
+    // put them back
+    ctx.putImageData(pixels, 0, 0 );
   }, 16);   // start every 16ms
 }
 
@@ -40,6 +45,16 @@ function takePhoto() {
   link.setAttribute('download', 'handsome');
   link.innerHTML = `<img src="${data}" alt="Handsome Man" />`;
   strip.insertBefore(link, strip.firstChild);
+}
+
+function redEffect(pixels) {
+  for(let i = 0; i < pixels.data.length; i+=4;) {
+    pixels.data[i] = pixels.data[i + 0] + 100;  // Red
+    pixels.data[i + 1] = pixels.data[i + 1] - 50;   // Green
+    pixels.data[i + 2] = pixels.data[i + 2] * 0.5;   // Blue
+  }
+
+  return pixels;
 }
 
 getVideo();
